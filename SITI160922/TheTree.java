121835236318eb02d872baada7be574d964dbcdd
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-10-22 04:59:27 +0700

    Create TheTree.java

---
+++ SITI160922/TheTree.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI160922;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public class TheTree {
+    public static void main(String[] args) {
+        System.out.println("I think that I shall never see,");
+        System.out.println("a poem as lovely as a tree. ");
+        System.out.println("A tree whose hungry mouth is pressed ");
+        System.out.println("Against the Earth’s sweet flowing breast.");
+    }
+}
+
+
+
