615e9d9da4114b606a1631d96f0a59cc0938e25d
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-10-22 04:59:25 +0700

    Create heloworld.java

---
+++ SITI160922/heloworld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI160922;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public class heloworld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming [Siti Nurhaliza Yus]!!!");
+    }
+}
