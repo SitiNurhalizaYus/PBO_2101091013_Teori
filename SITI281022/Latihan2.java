764e69e023343a71b251c73fb6910abd8f1c963f
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-10-28 07:18:41 +0700

    Create Latihan2.java

---
+++ SITI281022/Latihan2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SITI281022;
+
+/**
+ *
+ * @author siti nurhaliza yus
+ */
+public class Latihan2 {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+
+        int i=0;
+        do
+            {
+                System.out.println(hari[i]);
+                i++;
+            }while(i<hari.length);
+    }
+}  
